a97bb316ce2f709d7d6c996dce8327fb46554129
lucasmattos1 <lucas07dias@gmail.com>
2024-03-27 15:05:48 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let nome = "Lucas"
-console.log(nome);
+let nomeHeroi = "Superman"
+let xp = 1500
+
+while (xp < 1000){
+    xp = "Ferro"
+console.log("O Herói de nome " + (nomeHeroi) + " está no nível de " + (xp));
+break;
+}
+/*while (xp >= 1001 && xp <= 2000){
+    xp = "Bronze"
+console.log("O Herói de nome " + (nomeHeroi) + " está no nível de " + (xp));
+break;
+} */
